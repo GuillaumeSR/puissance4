e085676788790fff969e3cbb08961ee5b002bad5
laguigzzz <guillaume.samson-richert@coda-student.school>
2023-12-14 11:33:33 +0100

    adding diagonal win and changing col and row win, adding draw

--- script.js
+++ script.js
@@ -19,10 +19,11 @@ let l;
 
 let map = [];
 
-let rows = 9;
-let columns = 9;
+let rows = 5;
+let columns = 5;
 
 let win = false;
+let draw = false;
 
 function initializeMap() {
     for(i=0;i<rows;i++) {
@@ -87,6 +88,11 @@ function sessionStart() {
     showMap();
 }
 
+function reset() {
+    initializeMap();
+    showMap();
+}
+
 function nextTurn() {
     if (turn == player1) {
         turn = player2
@@ -101,8 +107,11 @@ function checkCol(x) {
             map[i][x] = turn;
             showMap();
             checkRowWin();
-            checkColWin()
+            checkColWin();
+            checkDiagWin();
             checkWin();
+            checkForDraw();
+            checkDraw();
             nextTurn();
             break;
         }
@@ -143,99 +152,155 @@ function checkWin() {
     }
 }
 
+function checkForDraw() {
+    // Check if any cell in the top row is empty
+    for (let c = 0; c < map[0].length; c++) {
+        if (map[0][c] === w) {
+            return;
+        }
+    }
+    draw = true
+}
+
+function checkDraw() {
+    if (draw == true) {
+        alert("Well played all but no winner this time !")
+    }
+}
+
 function checkRowWin() {
-    for (j=0;j<map[0].length;j++) {
-        let rp1 = 0;
-        let rp2 = 0;
-        for (i=map.length-1;i>=0;i--) {
-            if (map[i][j] != w) {
-                if (map[i][j] == player1) {
-                    rp1 += 1;
-                    rp2 = 0;
-                    console.log(rp1)
-                    if (rp1 == 4) {
-                        win = true;
-                        break;
-                    }
-                } else if (map[i][j] == player2) {
-                    rp2 += 1;
-                    rp1 = 0;
-                    console.log(rp2)
-                    if (rp2 == 4) {
-                        win = true;
-                        break;
-                    }
-                }
-            } else {
-                rp1 = 0;
-                rp2 = 0;
+    for (let r = 0; r < map.length; r++) {
+        for (let c = 0; c < map[0].length - 3; c++) {
+            if (map[r][c] !== w && map[r][c] === map[r][c + 1] && map[r][c] === map[r][c + 2] && map[r][c] === map[r][c + 3]) {
+                win = true;
             }
         }
     }
 }
 
 function checkColWin() {
-    for (j=0;j<map[0].length;j++) {
-        let cp1 = 0;
-        let cp2 = 0;
-        for (i=map.length-1;i>=0;i--) {
-            if (map[i][j] != w) {
-                if (map[i][j] == player1) {
-                    cp1 += 1;
-                    cp2 = 0;
-                    if (cp1 == 4) {
-                        win = true;
-                        break;
-                    }
-                } else if (map[i][j] == player2) {
-                    cp2 += 1;
-                    cp1 = 0;
-                    if (cp2 == 4) {
-                        win = true;
-                        break;
-                    }
-                }
-            } else {
-                cp1 = 0;
-                cp2 = 0;
+    for (let r = 0; r < map.length - 3; r++) {
+        for (let c = 0; c < map[0].length; c++) {
+            if (map[r][c] !== w && map[r][c] === map[r + 1][c] && map[r][c] === map[r + 2][c] && map[r][c] === map[r + 3][c]) {
+                win = true;
             }
         }
     }
 }
 
-function checkDiagUpRightWin() {
-    for (z=0;z<8;z++) {
-        let dp1 = 0;
-        let dp2 = 0;
-        let j = map[0].length;
-        let i = 0;
-        for (m=0;m<map.length-z;m++) {
-            if (map[i][j] != w) {
-                if (map[i][j] == player1) {
-                    dp1 += 1;
-                    dp2 = 0;
-                    if (dp1 == 4) {
-                        win = true;
-                        break;
-                    }
-                } else if (map[i][j] == player2) {
-                    dp2 += 1;
-                    dp1 = 0;
-                    if (dp2 == 4) {
-                        win = true;
-                        break;
-                    }
-                }
-            } else {
-                j -= 1;
-                i += 1;
+function checkDiagWin() {
+    for (let r = 3; r < map.length; r++) {
+        for (let c = 0; c < map[0].length - 3; c++) {
+            if (map[r][c] !== w && map[r][c] === map[r - 1][c + 1] && map[r][c] === map[r - 2][c + 2] && map[r][c] === map[r - 3][c + 3]) {
+                win = true;
             }
         }
-    for (z=1;z<map[0].length;z++) {
-        let dp1 = 0;
-        let dp2 = 0;
-        let j = map[0].length;
-        let i = 0;
     }
+    for (let r = 0; r < map.length - 3; r++) {
+        for (let c = 0; c < map[0].length - 3; c++) {
+            if (map[r][c] !== w && map[r][c] === map[r + 1][c + 1] && map[r][c] === map[r + 2][c + 2] && map[r][c] === map[r + 3][c + 3]) {
+                win = true;
+            }
+        }
     }
 }
+
+
+
+// function checkRowWin() {
+//     for (j=0;j<map[0].length;j++) {
+//         let rp1 = 0;
+//         let rp2 = 0;
+//         for (i=map.length-1;i>=0;i--) {
+//             if (map[i][j] != w) {
+//                 if (map[i][j] == player1) {
+//                     rp1 += 1;
+//                     rp2 = 0;
+//                     if (rp1 == 4) {
+//                         win = true;
+//                         break;
+//                     }
+//                 } else if (map[i][j] == player2) {
+//                     rp2 += 1;
+//                     rp1 = 0;
+//                     if (rp2 == 4) {
+//                         win = true;
+//                         break;
+//                     }
+//                 }
+//             } else {
+//                 rp1 = 0;
+//                 rp2 = 0;
+//             }
+//         }
+//     }
+// }
+
+// function checkColWin() {
+//     for (j=0;j<map[0].length;j++) {
+//         let cp1 = 0;
+//         let cp2 = 0;
+//         for (i=map.length-1;i>=0;i--) {
+//             if (map[i][j] != w) {
+//                 if (map[i][j] == player1) {
+//                     cp1 += 1;
+//                     cp2 = 0;
+//                     if (cp1 == 4) {
+//                         win = true;
+//                         break;
+//                     }
+//                 } else if (map[i][j] == player2) {
+//                     cp2 += 1;
+//                     cp1 = 0;
+//                     if (cp2 == 4) {
+//                         win = true;
+//                         break;
+//                     }
+//                 }
+//             } else {
+//                 cp1 = 0;
+//                 cp2 = 0;
+//             }
+//         }
+//     }
+// }
+
+// function checkDiagWinTest() {
+//     for (z=0;z<8;z++) {
+//         console.log(z)
+//         let dp1 = 0;
+//         let dp2 = 0;
+//         let j = map[0].length;
+//         let i = 0;
+//         for (m=0;m<map.length-z;m++) {
+//             if (map[i][j] != w) {
+//                 if (map[i][j] == player1) {
+//                     dp1 += 1;
+//                     dp2 = 0;
+//                     console.log(dp1);
+//                     if (dp1 == 4) {
+//                         win = true;
+//                         break;
+//                     }
+//                 } else if (map[i][j] == player2) {
+//                     dp2 += 1;
+//                     dp1 = 0;
+//                     console.log(dp2);
+//                     if (dp2 == 4) {
+//                         win = true;
+//                         break;
+//                     }
+//                 }
+//             } else {
+//                 j -= 1;
+//                 i += 1;
+//             }
+//         }
+//     for (z=1;z<map[0].length;z++) {
+//         let dp1 = 0;
+//         let dp2 = 0;
+//         let j = map[0].length;
+//         let i = 0;
+//     }
+//     }
+// }
